741d3bd6063074e18888ceb4a07c134ee7c67c02
dm7672 <dmitrsem10@gmail.com>
2026-01-19 20:40:45 +0500

    Добавлен класс Result

---
+++ TagsCloud/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TagsCloud
+{
+    public class None
+    {
+        private None()
+        {
+        }
+    }
+
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public T? Value { get; }
+        public string? Error { get; }
+
+        protected Result(bool isSuccess, T? value, string? error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T? value) => new Result<T>(true, value, null);
+        public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+        
+
+    }
+
+}
